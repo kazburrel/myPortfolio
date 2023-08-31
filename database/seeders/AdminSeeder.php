b8a5b4f35e43d877929511de87d85784a1ee1133
kazburrel <oegbosionu@gmail.com>
2023-08-31 14:36:35 +0100

    seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::factory()->create([
+            'first_name' => 'Obiora',
+            'last_name' => 'Egbosionu',
+            'email' => 'oegbosionu@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
+
+// ('first_name');
+//             $table->text('last_name');
+//             $table->text('email');
+//             $table->string('password');
+//             $table->string('password_confirmation');
